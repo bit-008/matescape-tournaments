a66579c5d5ee7af43f0d427aa319d9008f294769
8-Bit <90836727+bit-008@users.noreply.github.com>
2025-06-19 16:16:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -167,7 +167,7 @@ function App() {
         <div className="text-center mb-8">
           <img src={darkMode ? darkLogo : lightLogo} 
             alt="MATESCAPE TOURNAMENTS" 
-            className="mx-auto h-15 md:h-21 object-contain"
+            className="mx-auto h-15 md:h-20 object-contain"
           />
         </div>
 
